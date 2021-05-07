42ddd89203707aed2f6c8bdabda7024b72b27b00
edgardo <ing.edgardobolivar@gmail.com>
2021-05-07 00:21:39 -0500

    Se agrega mejora vista rapida de entradas

--- public/vistas/js/blog.js
+++ public/vistas/js/blog.js
@@ -1,6 +1,11 @@
 const RUTA_IMAGENES = '/uploads/images_blogs_entrities/';
 const TEMPLATE_ENTRY_PREVIEW = '<h3 class="text-center">{TITULO}</h3><img src="'+RUTA_IMAGENES+'{IMAGEN}" class="img-thumbnail mx-auto d-block" style="max-width:60%" /><strong>{AUTOR}</strong>/<span>{FECHA}</span><p>{TEXTO}</p><a class="text-danger" href="/ver-entrada/{ID}" title="Ver entrada completa">Ver todo</a>';
 
+setTimeout(function(){ 
+    console.log(document.getElementsByClassName("view-entry-preview")[0]);
+    document.getElementsByClassName("view-entry-preview")[0].click() 
+}, 100);
+
 function view_entry_preview(element) {
     const $this = $(element);
     
@@ -9,7 +14,7 @@ function view_entry_preview(element) {
     
     const image = $this.attr('entry_image') > '' ? $this.attr('entry_image') : 'empty.png';
     let Data = {
-        TITULO: $this.attr('entry_title'),
+        TITULO: $this.attr('entry_title').toUpperCase(),
         IMAGEN: image,
         AUTOR: $this.attr('entry_autor'),
         FECHA: $this.attr('entry_date'),
@@ -25,4 +30,6 @@ function view_entry_preview(element) {
 
     space_entry_preview.html(view);
 }
-// view_entry_preview(document.getElementsByClassName('view-entry-preview')[0]);
+
+
+
